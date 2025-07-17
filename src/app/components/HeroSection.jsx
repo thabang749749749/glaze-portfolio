fd03cb5837f2ee01204f7b61517dabb301f2e226
Thabang <you@example.com>
2025-07-17 14:21:32 +0200

    initial commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,60 @@
+"use client";
+import Image from 'next/image';
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+
+
+export const HeroSection = () => {
+  return (
+    <section>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"><span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Thabang",
+                1000,
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+            </h1>
+          <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga sit nam corporis exercitationem reprehenderit quia maxime.
+          </p>
+
+          <div>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-green-500 via-blue-500  to-purple-400 hover:bg-slate-800 cursor-pointer text-white'>
+              Hire Me
+            </button>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-600 via-purple-500 to-green-500 hover:bg-slate-800 text-white mt-3'>
+              <span className="block bg-[#121212] hover:bg-slate-800 cursor-pointer rounded-full px-5 py-2">Download CV</span>
+            </button>
+          </div>
+        </div >
+
+
+        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+          <div className='rounded-full bg-[#d6ffff] bg-opacity-20 w-[305px] h-[305px]  relative'>
+            <Image
+              src="/images/hero-image.JPG"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full "
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
