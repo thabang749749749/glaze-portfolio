644555b6f43347834cc096d980b5b9b8298cb640
Thabang <you@example.com>
2025-07-22 16:56:59 +0200

    Implement Project Section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,9 @@
 "use client";
-import Image from 'next/image';
-import React from 'react';
-import { TypeAnimation } from 'react-type-animation';
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 
 export const HeroSection = () => {
